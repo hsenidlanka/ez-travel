9194cb91b99fb1600c09924284295c233e425f5c
Menuka <menukaishan5@gmail.com>
2017-09-08 09:17:58 +0530

    Build failure corrections

--- modules/core-logic/src/main/java/hsenid/repository/user/customer/CustomerImpl.java
+++ modules/core-logic/src/main/java/hsenid/repository/user/customer/CustomerImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -27,6 +28,15 @@ public class CustomerImpl implements CustomerRepository{
         if (count > 0){
             return true;
         }
+
+        return false;
+    }
+
+    public boolean registerCustomer(String email, String password, String first_name, String last_name, Date birthday, String contact_number, String nic, String gender) {
+
+        String sql = "INSERT INTO customer (email, password, first_name, last_name, birthday, contact_number, nic, gender, user_status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, 1);";
+        jdbcTemplate.update(sql, email, password, first_name, last_name, birthday, contact_number, nic, gender);
+
 
         return false;
     }
