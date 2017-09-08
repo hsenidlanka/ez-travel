a41803bf78bdd0c8b44aa58950ad12469ffe195f
difnajayashani <difna4@gmail.com>
2017-09-08 18:04:36 +0530

    driver and passenger registration completed

--- modules/android_app/app/src/main/java/com/example/hsenid/taxiapp/RegistrationPassengerActivity.java
+++ modules/android_app/app/src/main/java/com/example/hsenid/taxiapp/RegistrationPassengerActivity.java
@@ -10,4 +10,6 @@ public class RegistrationPassengerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger_registration);
     }
+
+
 }
