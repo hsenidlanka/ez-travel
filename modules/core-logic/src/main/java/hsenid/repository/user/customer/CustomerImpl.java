3ab59f26aba02ae8496988ede62a634f21fa6316
Menuka <menukaishan5@gmail.com>
2017-09-08 17:58:43 +0530

    Registration Completed Post request to /api/customer/register request body should be consist
    email [String]
    password [String not hashed yet. waiting for front end to implement to refactor for SHA1]
    first_name [String]
    last_name [String]
    birthday [java.sql.Date should be 1991-03-16 format]
    contact_number [String]
    nic [String]
    gender [String]
    
    Not Server Side validations implemented.
    
    While Testing do not send same email address again. It'll fail the sql transaction.

--- modules/core-logic/src/main/java/hsenid/repository/user/customer/CustomerImpl.java
+++ modules/core-logic/src/main/java/hsenid/repository/user/customer/CustomerImpl.java
@@ -1,12 +1,15 @@
 package hsenid.repository.user.customer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.sql.Date;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by hsenid on 9/6/17.
@@ -19,6 +22,11 @@ public class CustomerImpl implements CustomerRepository{
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+
 
     public boolean isCustomerAuthenticated(String email, String password) {
 
@@ -34,10 +42,37 @@ public class CustomerImpl implements CustomerRepository{
 
     public boolean registerCustomer(String email, String password, String first_name, String last_name, Date birthday, String contact_number, String nic, String gender) {
 
-        String sql = "INSERT INTO customer (email, password, first_name, last_name, birthday, contact_number, nic, gender, user_status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, 1);";
-        jdbcTemplate.update(sql, email, password, first_name, last_name, birthday, contact_number, nic, gender);
+        TransactionDefinition def = new DefaultTransactionDefinition();
+        TransactionStatus status = transactionManager.getTransaction(def);
 
+        try {
+            String sql = "INSERT INTO customer (email, password, first_name, last_name, birthday, contact_number, nic, gender, user_status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, 1);";
+            jdbcTemplate.update(sql, email, password, first_name, last_name, birthday, contact_number, nic, gender);
+
+            System.out.println("Query 1 completed");
+
+            String getCustomerId = "SELECT customer_id FROM customer WHERE email = ? LIMIT 1";
+            String customerId = jdbcTemplate.queryForObject(getCustomerId, new Object[]{email}, String.class);
+
+
+            System.out.println("Customer id =>" + customerId);
+//            System.out.println(customerId);
+            System.out.println("Query 2 completed");
+
+            String defaultLocationSql = "INSERT INTO customer_current_location (customer_id, longitude, latitude, update_time) VALUES (?, ?, ?, NOW())";
+            jdbcTemplate.update(defaultLocationSql, customerId, 0.000000, 0.000000);
+            System.out.println("Query 3 completed");
+
+            transactionManager.commit(status);
+
+            return true;
+        } catch (DataAccessException e) {
+            System.out.println("Exception msg => " + e.getMessage());
+            transactionManager.rollback(status);
+        }
 
         return false;
     }
+
+
 }
