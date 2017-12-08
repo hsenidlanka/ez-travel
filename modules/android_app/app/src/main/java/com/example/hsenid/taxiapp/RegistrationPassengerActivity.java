f08fefc5494b3b7c2180e4843fb53046f1112a40
difnajayashani <difna4@gmail.com>
2017-12-08 09:54:09 +0530

    passenger registration activity completed

--- modules/android_app/app/src/main/java/com/example/hsenid/taxiapp/RegistrationPassengerActivity.java
+++ modules/android_app/app/src/main/java/com/example/hsenid/taxiapp/RegistrationPassengerActivity.java
@@ -1,13 +1,30 @@
 package com.example.hsenid.taxiapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class RegistrationPassengerActivity extends AppCompatActivity {
+
+    Button passengerRegisterConfirmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_passenger_registration);
+
+
+        //on the click of Signin button
+        passengerRegisterConfirmButton = (Button) findViewById(R.id.signup_register_passenger);
+        passengerRegisterConfirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //attemptLogin();
+            }
+        });
     }
+
+
+
 }
